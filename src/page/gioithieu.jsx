f997e4d6797b98e449e52693c673c4cc9c616cbc
TuilaTammmmm <dragonkiller2k5@gmail.com>
2026-05-22 00:16:17 +0700

    hoàn thiện các trang

--- src/page/gioithieu.jsx
+++ src/page/gioithieu.jsx
@@ -1,6 +1,31 @@
+import React, { useState, useEffect } from 'react';
+import { Breadcrumb } from 'react-bootstrap';
+import axios from 'axios';
+
 function Gioithieu() {
+    const [ndgioithieu, setndgioithieu] = useState({});
+
+    useEffect(() => {
+        axios.get('http://localhost:9999/pagesText')
+            .then(res => {
+                setndgioithieu(res.data.gioithieu);
+            })
+            .catch(err => console.error("Lỗi:", err));
+    }, []);
     return (
-        <></>
+        <div>
+            <Breadcrumb>
+                <Breadcrumb.Item href="/" linkProps={{ className: 'text-dark text-decoration-none' }}>Trang chủ</Breadcrumb.Item>
+                <Breadcrumb.Item active>Giới thiệu</Breadcrumb.Item>
+            </Breadcrumb>
+            <h1 className="text-primary mb-3">
+                {ndgioithieu.title}
+            </h1>
+
+            <p className="fs-5" style={{ whiteSpace: 'pre-line' }}>
+                {ndgioithieu.content}
+            </p>
+        </div>
     )
 }
 
